f7d42546bb65fbb27c6d1de513c686c23f1e0fbb
Kohei-Ogino <kokyu9696@gmail.com>
2021-03-17 21:24:15 +0900

    Fix

--- Setup/setup_ros.bash
+++ Setup/setup_ros.bash
@@ -8,17 +8,18 @@ git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu18.04_desktop
 cd ros_setup_scripts_Ubuntu18.04_desktop
 sudo ./step1.bash
 source ~/.bashrc
+sudo rosdep fix-permissions
 
 # Create catkin_ws
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws
-cat ~/.bashrc
-source ~/.bashrc
-source /opt/ros/melodic/setup.bash
-echi "##########"
-echo $ROS_VERSION
-echi "##########"
-cd ~/catkin_ws/src
-catkin_init_workspace
-cd ~/catkin_ws
-catkin_make
+#mkdir -p ~/catkin_ws/src
+#cd ~/catkin_ws
+#cat ~/.bashrc
+#source ~/.bashrc
+#source /opt/ros/melodic/setup.bash
+#echi "##########"
+#echo $ROS_VERSION
+#echi "##########"
+#cd ~/catkin_ws/src
+#catkin_init_workspace
+#cd ~/catkin_ws
+#catkin_make
