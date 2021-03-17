b7bf062dd2833655d3376037fbcfbf739af6bbaf
Kohei-Ogino <kokyu9696@gmail.com>
2021-03-17 19:58:10 +0900

    Fix

--- Setup/setup_ros.bash
+++ Setup/setup_ros.bash
@@ -10,8 +10,16 @@ sudo ./step1.bash
 source ~/.bashrc
 
 # Create catkin_ws
-#mkdir -p ~/catkin_ws/src
-#cd ~/catkin_ws
-#cat ~/.bashrc
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws
+cat ~/.bashrc
+source ~/.bashrc
+source /opt/ros/melodic/setup.bash
+echo "######################################################################"
+echo $ROS_VERSION
+cd ~/catkin_ws/src
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
 #roscore
 #catkin_make
