7405b881b4f0167a164bce561bea277657d9646e
Kohei-Ogino <kokyu9696@gmail.com>
2021-03-18 12:15:24 +0900

    Fix sucess

--- Setup/setup_simulator.bash
+++ Setup/setup_simulator.bash
@@ -7,7 +7,10 @@ git clone https://github.com/RoboticaUtnFrba/create_autonomy.git
 git clone https://github.com/RoboticaUtnFrba/libcreate.git
 
 cd ~/catkin_ws
-sudo ./src/create_autonomy/sensors/ca_imu/scripts/install_rtimulib.sh
+#sudo apt install libxml2-dev libxslt-dev
+#sudo apt install qtbase5-dev qttools5-dev-tools qt5-default
+sudo apt install libqt4-dev libqt4-dev-bin libqt4-opengl-dev libqtwebkit-dev qt4-linguist-tools qt4-qmake
+./src/create_autonomy/sensors/ca_imu/scripts/install_rtimulib.sh
 cd ~/catkin_ws
 sudo apt install -y python3-vcstool
 vcs import src < src/create_autonomy/dependencies.repos
@@ -23,5 +26,8 @@ source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 
 source ~/.bashrc
 
+
 # Launch simulator
+# cd ~/catkin_ws && catkin_make
 # roslaunch ca_gazebo create_empty_world.launch
+# roslaunch ca_tools keyboard_teleop.launch
