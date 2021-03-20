08caadd1359e824dac872cc249cd88f09804fa53
KOHE1 <55177338+KOHE11@users.noreply.github.com>
2021-03-20 19:38:45 +0900

    Update setup_ros.bash

--- Setup/setup_ros.bash
+++ Setup/setup_ros.bash
@@ -8,7 +8,9 @@ git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu18.04_desktop
 cd ros_setup_scripts_Ubuntu18.04_desktop
 sudo ./step1.bash
 source ~/.bashrc
+
 sudo rosdep fix-permissions
+rosdep update
 
 # Create catkin_ws
 mkdir -p ~/catkin_ws/src
